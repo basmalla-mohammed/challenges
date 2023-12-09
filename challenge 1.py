d57b7d8a8ace5908ce55112751735e4614582014
basmallamohammed <basmallamohamed1392003@gmail.com>
2023-12-09 15:06:45 +0200

    hi

--- challenge 1.py
+++ challenge 1.py
@@ -5,4 +5,4 @@ print(message) #print This is going to be tricky ;
 result = 3**2
 print("3**2 =", result) #print 3**2=6
 result = 3 - 5
-print("Challenge complete!") #print challenge complete
+print("Challenge complete!") #print challenge complet
