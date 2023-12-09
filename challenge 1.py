b8f13d92b06a996d06c492a5b0fdda749b38dd7d
basmallamohammed <basmallamohamed1392003@gmail.com>
2023-12-09 20:16:39 +0200

    myyyyy

--- challenge 1.py
+++ challenge 1.py
@@ -1,7 +1,7 @@
 print("Challenge 1:") # print challeng1 
 message = "This is going to be tricky ;)"
 Message = "Very tricky!"
-print(message) #print This is going to be tricky ;
+print(message) #print This is going to be tricky;
 result = 3**2
 print("3**2 =", result) #print 3**2=6;
 result = 3 - 5
